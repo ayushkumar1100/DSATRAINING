d102bb71f24fce5f235bd9252642cae878f80e31
ayushkumar1100 <ayushkumar4010@gmail.com>
2026-06-25 14:29:59 +0530

    valid anagram code with approach explanation.

---
+++ Strings/ReverseWords.java
@@ -0,0 +1,7 @@
+public class TraverseWords{
+    public static void main(String[] args){
+        String sen = "My name is Ayush.";
+        
+
+    }
+}
---
+++ Strings/ValidAnagrams.java
@@ -0,0 +1,30 @@
+public class ValidAnagrams {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nvgaram";
+
+        System.out.println(isAnagram(s, t));
+    }
+}
